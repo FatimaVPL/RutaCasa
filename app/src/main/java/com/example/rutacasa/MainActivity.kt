29c720d106b21662c0095ac59133e41e34b3ae8b
Gaby_Perez <monseperez267@gmail.com>
2023-04-15 23:49:24 -0600

    Markers End and Start

--- app/src/main/java/com/example/rutacasa/MainActivity.kt
+++ app/src/main/java/com/example/rutacasa/MainActivity.kt
@@ -1,9 +1,13 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.rutacasa
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.location.LocationManager
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
@@ -16,51 +20,103 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.*
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
-    private var firstMarker: Marker? = null
     private lateinit var btnVerRuta: Button
     private var map: MapView? = null
     private var start: String = ""
     private var end: String = ""
 
-
-    //your items
-    var items = ArrayList<OverlayItem>()
-
-    @SuppressLint("MissingInflatedId")
+    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility", "MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         val ctx: Context = applicationContext
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
 
         setContentView(R.layout.activity_main)
 
-        //Proveedor de mapas
+        //Map Provider
         map = findViewById<View>(R.id.map) as MapView
         map!!.setTileSource(TileSourceFactory.MAPNIK)
 
-        //Mi ubicación
-        var ubicacion = MyLocationNewOverlay(GpsMyLocationProvider(applicationContext), map)
-        ubicacion.enableMyLocation()
+        //Current Location
+        //val ubicacion = MyLocationNewOverlay(GpsMyLocationProvider(applicationContext), map)
+        //ubicacion.enableMyLocation()
+        //map!!.overlays.add(ubicacion)
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationProvider = LocationManager.GPS_PROVIDER
+        val currentLocation = locationManager.getLastKnownLocation(locationProvider)
+
+        //MapController
         val mapController = map!!.controller
-        mapController.setZoom(9)
-        val startPoint = GeoPoint(20.4791291,-100.7582735)
+        mapController.setZoom(15)
+        val startPoint = GeoPoint(20.139398208378335, -101.15073143396242)
         mapController.setCenter(startPoint)
-        map!!.getOverlays().add(ubicacion)
+
+        //Add markers
+        val markerStart = Marker(map)
+        markerStart.isDraggable = true
+        markerStart.position = currentLocation?.let { GeoPoint(it.latitude, currentLocation.longitude) }
+        markerStart.title = "Punto Inicio"
+        map!!.overlays.add(markerStart)
+
+        val markerEnd = Marker(map)
+        markerEnd.isDraggable = true
+        markerEnd.position = GeoPoint(20.139398208378335, -101.15073143396242)
+        markerEnd.title = "Punto Destino"
+        map?.overlays?.add(markerEnd)
+
+        markerStart.setOnMarkerDragListener(object : Marker.OnMarkerDragListener {
+            override fun onMarkerDragStart(marker: Marker) {
+
+            }
+
+            override fun onMarkerDrag(marker: Marker) {
+
+            }
+
+            override fun onMarkerDragEnd(marker: Marker) {
+                Log.d("MarkerStart", "Nueva posición del marcador: ${marker.position.latitude}, ${marker.position.longitude}")
+            }
+        })
+
+        markerEnd.setOnMarkerDragListener(object : Marker.OnMarkerDragListener {
+            override fun onMarkerDragStart(marker: Marker) {
+
+            }
+
+            override fun onMarkerDrag(marker: Marker) {
+
+            }
+
+            override fun onMarkerDragEnd(marker: Marker) {
+                Log.d("MarkerEnd", "Nueva posición del marcador: ${marker.position.latitude}, ${marker.position.longitude}")
+            }
+        })
 
 
+        /*
+        // Click on map
+        map!!.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                // Coordinates
+                val clickedPoint = map!!.projection.fromPixels(event.x.toInt(), event.y.toInt()) as GeoPoint
 
+                //Log.d("MapClick", "Latitude: ${clickedPoint.latitude}, Longitude: ${clickedPoint.longitude}")
 
+                marker = Marker(map)
+                marker?.position = clickedPoint
+                marker?.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
+                map?.overlays?.add(marker)
+            }
+            true
+        }*/
 
-        // Botón para mostrar ruta
+        // Button show rute
         btnVerRuta = findViewById(R.id.btnCalcularRuta)
 
         btnVerRuta.setOnClickListener{
@@ -102,7 +158,7 @@ class MainActivity : AppCompatActivity() {
         firstMarker = Marker(map)
         firstMarker?.position = startPoint
         firstMarker?.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
-        firstMarker?.title = "Bello ITSUR"
+        firstMarker?.title = "Title"
         map?.overlays?.add(firstMarker)
 */
         map?.invalidate()
@@ -125,7 +181,7 @@ class MainActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             val call = getRetrofit().create(ApiService::class.java).getRoute("5b3ce3597851110001cf62488d38aa048bea4519ae3177df424c06de", start, end)
             if (call.isSuccessful) {
-                //Aquí se pintará la ruta
+                //Show rute
                 drawRoute(call.body())
             } else {
 
