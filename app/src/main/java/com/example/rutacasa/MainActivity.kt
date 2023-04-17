0d46dbe09a225b46a016f31fcbf17470c09c35d7
Gaby_Perez <monseperez267@gmail.com>
2023-04-16 22:32:43 -0600

    Clear button and show rute

--- app/src/main/java/com/example/rutacasa/MainActivity.kt
+++ app/src/main/java/com/example/rutacasa/MainActivity.kt
@@ -7,12 +7,9 @@ import android.content.Context
 import android.location.LocationManager
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.util.Log
-import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -21,19 +18,19 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.*
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnVerRuta: Button
     private var map: MapView? = null
+    private var line = Polyline()
     private var start: String = ""
     private var end: String = ""
     private val apiService: ApiService by lazy {
         Directions.apiService
     }
-    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility", "MissingPermission")
+    @SuppressLint("MissingInflatedId", "ClickableViewAccessibility", "MissingPermission",
+        "SetTextI18n"
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -47,12 +44,9 @@ class MainActivity : AppCompatActivity() {
         map!!.setTileSource(TileSourceFactory.MAPNIK)
 
         //Current Location
-        //val ubicacion = MyLocationNewOverlay(GpsMyLocationProvider(applicationContext), map)
-        //ubicacion.enableMyLocation()
-        //map!!.overlays.add(ubicacion)
-        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val locationProvider = LocationManager.GPS_PROVIDER
-       // val currentLocation = locationManager.getLastKnownLocation(locationProvider)
+        //val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        //val locationProvider = LocationManager.GPS_PROVIDER
+        //val currentLocation = locationManager.getLastKnownLocation(locationProvider)
 
         //MapController
         val mapController = map!!.controller
@@ -63,13 +57,14 @@ class MainActivity : AppCompatActivity() {
         //Add markers
         val markerStart = Marker(map)
         markerStart.isDraggable = true
-       // markerStart.position = currentLocation?.let { GeoPoint(it.latitude, currentLocation.longitude) }
-       // markerStart.title = "Punto Inicio"
-      //  map!!.overlays.add(markerStart)
+        //markerStart.position = currentLocation?.let { GeoPoint(it.latitude, currentLocation.longitude) }
+        markerStart.position = GeoPoint(20.139398208378335, -101.15073143396242)
+        markerStart.title = "Punto Inicio"
+        map!!.overlays.add(markerStart)
 
         val markerEnd = Marker(map)
         markerEnd.isDraggable = true
-        markerEnd.position = GeoPoint(20.139398208378335, -101.15073143396242)
+        markerEnd.position = GeoPoint(20.118886642184798, -101.17034500679607)
         markerEnd.title = "Punto Destino"
         map?.overlays?.add(markerEnd)
 
@@ -83,7 +78,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onMarkerDragEnd(marker: Marker) {
-                Log.d("MarkerStart", "Nueva posición del marcador: ${marker.position.latitude}, ${marker.position.longitude}")
+                //Log.d("MarkerStart", "${marker.position.latitude}, ${marker.position.longitude}")
             }
         })
 
@@ -97,75 +92,27 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onMarkerDragEnd(marker: Marker) {
-                Log.d("MarkerEnd", "Nueva posición del marcador: ${marker.position.latitude}, ${marker.position.longitude}")
+                //Log.d("MarkerEnd", "${marker.position.latitude}, ${marker.position.longitude}")
             }
         })
 
-
-        /*
-        // Click on map
-        map!!.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                // Coordinates
-                val clickedPoint = map!!.projection.fromPixels(event.x.toInt(), event.y.toInt()) as GeoPoint
-
-                //Log.d("MapClick", "Latitude: ${clickedPoint.latitude}, Longitude: ${clickedPoint.longitude}")
-
-                marker = Marker(map)
-                marker?.position = clickedPoint
-                marker?.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
-                map?.overlays?.add(marker)
-            }
-            true
-        }*/
-
-        // Button show rute
         btnVerRuta = findViewById(R.id.btnCalcularRuta)
+        var aux = 0
 
         btnVerRuta.setOnClickListener{
-
-           drawRoute()
-            map!!.setOnTouchListener(object : View.OnTouchListener {
-                override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                    if (event?.action == MotionEvent.ACTION_UP) {
-                        //Click
-                        if (start.isEmpty()) {
-                            start = "${map!!.mapCenter.latitude},${map!!.mapCenter.longitude}"
-                        } else if (end.isEmpty()) {
-                            end = "${map!!.mapCenter.latitude},${map!!.mapCenter.longitude}"
-
-                        }
-                        return true
-                    }
-                    return false
-                }
-            })
+            if (aux == 0) {
+                line = Polyline()
+                drawRoute(markerStart.position, markerEnd.position)
+                btnVerRuta.text = "Limpiar"
+                aux++
+            } else {
+                btnVerRuta.text = "Ver ruta"
+                map?.overlays?.remove(line)
+                aux = 0
+            }
         }
 
-
-     /*   val mapController = map!!.controller
-        mapController.setZoom(19)
-
-
-        val startPoint = GeoPoint(20.140153689100682, -101.15067778465794)
-        mapController.setCenter(startPoint)
-
-        items.add(
-            OverlayItem(
-                "Title",
-                "Description",
-                GeoPoint(0.0, 0.0)
-            )
-        ) // Lat/Lon decimal degrees
-
-        firstMarker = Marker(map)
-        firstMarker?.position = startPoint
-        firstMarker?.setAnchor(Marker.ANCHOR_BOTTOM, Marker.ANCHOR_CENTER)
-        firstMarker?.title = "Title"
-        map?.overlays?.add(firstMarker)
-*/
         map?.invalidate()
-
     }
 
     override fun onResume() {
@@ -181,22 +128,23 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun drawRoute(){
+    private fun drawRoute(startPoint: GeoPoint, endPoint: GeoPoint){
         CoroutineScope(Dispatchers.IO).launch {
-            val coor = apiService.getRoute("5b3ce3597851110001cf62488d38aa048bea4519ae3177df424c06de", start, end)
-            val features = coor.features
-            val line = Polyline()
-            val startPoint = GeoPoint(20.139398208378335, -101.15073143396242)
-            val endPoint = GeoPoint(20.1433984,-101.1546329)
-             end = "${endPoint!!.longitude},${endPoint!!.latitude}"
-            start = "${startPoint!!.longitude},${startPoint!!.latitude}"
+            //val startPoint = GeoPoint(20.139398208378335, -101.15073143396242)
+            //val endPoint = GeoPoint(20.118886642184798, -101.17034500679607)
+            end = "${endPoint.longitude},${endPoint.latitude}"
+            start = "${startPoint.longitude},${startPoint.latitude}"
+
+            val points = apiService.getRoute("5b3ce3597851110001cf62488d38aa048bea4519ae3177df424c06de", start, end)
+            val features = points.features
+            //val line = Polyline()
 
             for (feature in features) {
                 val geometry = feature.geometry
                 val coordinates = geometry.coordinates
                 for (coordinate in coordinates) {
-                    val punto = GeoPoint(coordinate[1], coordinate[0])
-                    line.addPoint(punto)
+                    val point = GeoPoint(coordinate[1], coordinate[0])
+                    line.addPoint(point)
                 }
                 map?.overlays?.add(line)
             }
